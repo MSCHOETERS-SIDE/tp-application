64902e274fecb437f600626c95513924807f0b2b
mschoeters <m.schoeters@thomas-piron.eu>
2022-10-17 12:28:14 +0200

    test

--- TP_FormationGit/Controllers/WeatherForecastController.cs
+++ TP_FormationGit/Controllers/WeatherForecastController.cs
@@ -10,7 +10,7 @@ namespace TP_FormationGit.Controllers
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
-
+//test
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
